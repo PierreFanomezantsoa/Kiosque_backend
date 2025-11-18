6dd963690a8e353faf1c7430e68dd9a8212a5582
Pierre <rnandrasanarivopierre@gmail.com>
2025-11-18 20:55:27 +0300

    commit 18/11/2025 20:55

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\MenuItem;
-use App\Repository\MenuItemRepository; // ← à ajouter
+use App\Repository\MenuItemRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -25,7 +25,7 @@ final class MenuController extends AbstractController
                 'name' => $item->getName(),
                 'description' => $item->getDescription(),
                 'price' => $item->getPrice(),
-                'image' => $item->getImage() // URL relative ou absolute
+                'image' => $item->getImage()
             ];
         }
 
@@ -60,5 +60,50 @@ final class MenuController extends AbstractController
                 'image' => $item->getImage(),
             ]
         ], 201);
+    }
+
+    // 🔹 Méthode PUT pour mettre à jour un menu existant
+    #[Route('/{id}', name: 'update', methods: ['PUT'])]
+    public function update(int $id, Request $request, EntityManagerInterface $em, MenuItemRepository $repo): JsonResponse
+    {
+        $item = $repo->find($id);
+        if (!$item) {
+            return new JsonResponse(['error' => 'Plat non trouvé'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['name'])) $item->setName($data['name']);
+        if (isset($data['description'])) $item->setDescription($data['description']);
+        if (isset($data['price'])) $item->setPrice($data['price']);
+        if (array_key_exists('image', $data)) $item->setImage($data['image']);
+
+        $em->flush();
+
+        return $this->json([
+            'message' => 'Plat mis à jour',
+            'item' => [
+                'id' => $item->getId(),
+                'name' => $item->getName(),
+                'description' => $item->getDescription(),
+                'price' => $item->getPrice(),
+                'image' => $item->getImage(),
+            ]
+        ]);
+    }
+
+    // 🔹 Méthode DELETE pour supprimer un plat
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(int $id, EntityManagerInterface $em, MenuItemRepository $repo): JsonResponse
+    {
+        $item = $repo->find($id);
+        if (!$item) {
+            return new JsonResponse(['error' => 'Plat non trouvé'], 404);
+        }
+
+        $em->remove($item);
+        $em->flush();
+
+        return $this->json(['message' => 'Plat supprimé']);
     }
 }
